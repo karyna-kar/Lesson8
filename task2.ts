cbf2dec972c26d84d9195dc3556a85ffd4d560b5
Karina <karyna.karmaza@gmail.com>
2026-02-09 20:10:01 +0200

    feat: add solution for task2

--- task2.ts
+++ task2.ts
@@ -2,3 +2,14 @@
 // Написать функцию, которая складывает все четные числа и возвращает результат
 
 const array: number[] = [4, 33, 45, 20, 57, 0, 2];
+
+const sumEvenArray = (arr: number[]) => {
+  return arr.reduce((acc, value) => {
+    if (!(value % 2)) {
+      return acc + value;
+    }
+    return acc;
+  }, 0);
+};
+
+console.log(sumEvenArray(array));
